8f63acb6f41a8784a30503457d25f4c179bd2191
gdodd1 <garrettdodd03@gmail.com>
2022-02-20 04:35:13 -0600

    fix css

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "../styles/Calendar.css";
-import pic from "../assets/LeaveYourLEGACY2.png";
+//import pic from "../assets/LeaveYourLEGACY2.png";
 //fix background pic
 function Calendar() {
   return (
-    <div>
+    <div className='calendarDiv'>
         <a href="https://calendar.google.com/calendar/embed?src=r0kmjstvbs47549842g2ivc1oc%40group.calendar.google.com&ctz=America%2FChicago"
         target="_blank" rel="noreferrer">
-            <h1>
+            <h1 className='calendarH1'>
                 Click here for access to the Google Calendar
             </h1>
         </a>
